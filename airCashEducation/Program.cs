3f47a2bd1c1990cadaff66f5f950d4baf53d67cf
WIN10 <WIN10@DESKTOP-S1ARAKR>
2021-09-14 16:25:40 +0200

    Add project files.

---
+++ airCashEducation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace airCashEducation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
