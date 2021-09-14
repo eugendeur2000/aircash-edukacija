90fdd1e3ac77f71f1612e48b68689e54f60ce8d9
eugen deur <eugendeur0000@gmail.com>
2021-09-14 17:13:13 +0200

    Last task

--- airCashEducation/Program.cs
+++ airCashEducation/Program.cs
@@ -6,7 +6,29 @@ namespace airCashEducation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int rb = 6;
+            int b = 7;
+            Console.WriteLine(rb);
+            Console.WriteLine(b);
+            Console.Write("unesite realni broj");
+            int realniBroj = int.Parse(Console.ReadLine());
+
+            switch(realniBroj)
+            {
+                case 1:
+                    Console.WriteLine("zbrajanje");
+                    break;
+                case 2:
+                    Console.WriteLine("oduzimanje");
+                    break;
+                case 3:
+                    Console.WriteLine("množenje");
+                    break;
+                case 4:
+                    Console.WriteLine("dijeljenje");
+                    break;
+            }
+            Console.WriteLine("kraj programa");
         }
     }
 }
