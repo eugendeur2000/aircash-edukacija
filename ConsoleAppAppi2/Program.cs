5f87f820649a0a514a5ab5a5da66f3d5157f002f
eugen deur <eugendeur0000@gmail.com>
2021-12-21 16:25:01 +0100

    New Appi

---
+++ ConsoleAppAppi2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+namespace ConsoleAppAppi2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Main().ConfigureAwait(true).GetAwaiter().GetResult();
+        }
+        static async Task Main()
+        {
+
+            using var client = new HttpClient();
+            var result = await client.GetStringAsync("https://imdb-api.com/API/AdvancedSearch/k_r9c3h40j/?genres=action,adventure");
+        }
+    }
+}
